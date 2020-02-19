b504218e1493169221506fa05ea84800119c6a44
Saber GRAF <saberpharm@yahoo.fr>
2020-02-19 12:51:08 +0100

    add auth blueprint

--- project/api/models.py
+++ project/api/models.py
@@ -31,7 +31,9 @@ class User(db.Model):
         try:
             payload = {
                 'exp': datetime.datetime.utcnow() + datetime.timedelta(
-                    days=0, seconds=5),
+                    days=current_app.config.get('TOKEN_EXPIRATION_DAYS'),
+                    seconds=current_app.config.get('TOKEN_EXPIRATION_SECONDS')
+                ),
                 'iat': datetime.datetime.utcnow(),
                 'sub': user_id
             }
@@ -42,3 +44,17 @@ class User(db.Model):
             )
         except Exception as e:
             return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token - :param auth_token: - :return: integer|string
+        """
+        try:
+            payload = jwt.decode(
+                auth_token, current_app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
