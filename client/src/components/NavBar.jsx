32bcc5d84e27eff394b8642401f8dc4259b2c4bd
Saber GRAF <saberpharm@yahoo.fr>
2020-02-20 12:33:30 +0100

    add form component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const NavBar = props => (
+  <Container>
+    <Navbar collapseOnSelect bg="light" expand="lg">
+      <Navbar.Brand href="/"><span>{props.title}</span></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/status">Status</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/register">Register</Nav.Link>
+          <Nav.Link href="/login">Log In</Nav.Link>
+          <Nav.Link href="/logout">Log Out</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Container>
+);
+
+export default NavBar;
